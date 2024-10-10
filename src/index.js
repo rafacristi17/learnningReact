8c43363b1773f990d0f02e7ed67271f52d17896c
RAFAELA CRISTINE CUNHA LIMA <RAFAELA4438@IOT>
2024-10-10 20:00:17 -0300

    incluindo conteudos da aula de hoje

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const myElement = React.createElement('h1', {}, 'I do not use  JSX!');
+// voce pode codar de divesas formas, segue algumas abaixo, para verificar como fica basta descomentar e rodar o código 
+const myElement = <h1 className="myclass">Rafaela</h1>
+/*(
+  <>
+  <p>I am paragraph</p>
+  <p>I am paragraph too.</p>
+  </>
+)*/
+/*(<ul>
+  <li>Apples</li>
+  <li>bananas</li>
+  <li>cherrys</li>
+</ul>
+)*/
+// <h1>React is{5+5}times better with jsx</h1>
+// <h1>Mrs. Rafaela</h1>
+// React.createElement('h1', {}, 'I do not use  JSX!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
